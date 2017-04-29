a1fddd48e2277a445259467eda8bedb26cddbe63
Arnau Infante <arnauinfantepinos@gmail.com>
2017-04-29 18:58:14 +0200

    bla bla bla de android y estos comentarios no sirven de nada xD

--- SnapfitFinal/Snapfit/app/src/main/java/com/example/admin/prova/SettingsActivity.java
+++ SnapfitFinal/Snapfit/app/src/main/java/com/example/admin/prova/SettingsActivity.java
@@ -1,5 +1,7 @@
 package com.example.admin.prova;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -55,6 +58,56 @@ public class SettingsActivity extends AppCompatActivity {
                         startActivity(passwords);
                         break;
 
+
+                }
+            }
+        });
+
+        list_account.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i)
+                {
+                    case 0:
+
+                        break;
+                    case 1:
+
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+
+                        builder.setTitle("Log out");
+                        builder.setMessage("Are you sure you want to exit?");
+
+                        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                // Do nothing but close the dialog
+
+                                dialog.dismiss();
+                                Intent logOut = new Intent(SettingsActivity.this, MainActivity.class);
+                                Toast.makeText(getApplicationContext(),"",Toast.LENGTH_LONG).show();
+                                startActivity(logOut);
+
+                            }
+                        });
+
+                        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                // Do nothing
+                                dialog.dismiss();
+                            }
+                        });
+
+                        AlertDialog alert = builder.create();
+                        alert.show();
+
+
+                        break;
 
                 }
             }
