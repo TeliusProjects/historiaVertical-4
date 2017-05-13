8dc4456b5eddf080375fe3f13ad6ea06e0a0f025
Arnau Infante <arnauinfantepinos@gmail.com>
2017-05-13 15:55:00 +0200

    actualiza rest

---
+++ REST/modify.php
@@ -0,0 +1,80 @@
+<?php
+ require ('/home/snapfit/vendor/autoload.php');
+
+ $entityBody = file_get_contents('php://input');
+ $body = json_decode($entityBody,true);
+
+    $user_found = null;
+    $usercorrect = false;
+
+ if(empty($body))
+ {
+    $username    = $_POST['username'];
+ 	$oldPassword = md5($_POST['oldPassword']);
+    $newPassword = md5($_POST['newPassword']);
+
+
+ }
+ else
+ {
+ 	header('Content-type: application/json');
+
+ 	foreach ($body as $value) 
+    {
+        $username    = $value['username'];
+        $oldPassword = $value['oldPassword'];
+        $newPassword = $value['newPassword'];
+ 	}
+ }
+
+  $con = new MongoDB\Client;
+    // Select Database
+    if($con){
+        $db = $con->users;
+        // Select Collection
+        $collection = $db->user;
+        $filter = array(['Username' => $username,
+                         'Password' => $oldPassword
+                         ]);
+        $options = [
+
+            'projection' => ['Username' => 1], ['Password' => 1]
+        ];
+
+        $qry = new MongoDB\Driver\Query($filter,$options);
+       
+
+        $rows = $collection->find($qry);
+
+        foreach($rows as $row)
+        {
+
+            if($row['Username']== $username && $row['Password'] == $oldPassword)
+            {
+                $criteria = ['Username' => $row['Username']];
+                $newData = ['$set' => ['Password' => $newPassword]];
+
+                $collection -> updateOne($criteria,$newData);
+                $usercorrect = true;
+            }
+            
+        }
+
+
+        if ($usercorrect)
+        {
+            $changed=true;
+            echo json_encode(array('status'=> '1','isChanged' => $changed));
+
+        }else
+        {
+            $changed = false;
+              echo json_encode(array('status'=> '2','isChanged' => $changed));   
+        }
+ }
+ else
+ {
+        die("Mongo DB not connected!");
+    }
+  
+?>
---
+++ REST/modifyProfile.php
@@ -0,0 +1,110 @@
+<?php
+require ('/home/snapfit/vendor/autoload.php');
+
+ $entityBody = file_get_contents('php://input');
+ $body = json_decode($entityBody,true);
+
+    $user_found = null;
+    $usercorrect = false;
+
+ if(empty($body))
+ {
+    $username    = $_POST['username'];
+ 	  $email       = $_POST['email'];
+    $name        = $_POST['name'];
+    $birthday    = $_POST['birthday'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $gender      = $_POST['gender'];
+    
+
+
+ }
+ else
+ {
+ 	header('Content-type: application/json');
+
+ 	foreach ($body as $value) 
+    {
+        $username    = $value['username'];
+ 	      $email       = $value['email'];
+        $name        = $value['name'];
+        $birthday    = $value['birthday'];
+        $phoneNumber = $value['phoneNumber'];
+        $gender      = $value['gender'];
+ 	}
+ }
+ $con = new MongoDB\Client;
+
+ if($con)
+ {
+   $db = $con->users;
+   $collection= $db->user;
+   $filter = array('Username'=>$username);
+   $options= [ 'projection' => ['Username'=> 1]];
+  
+   $qry = new MongoDB\Driver\Query($filter,$options);
+
+   $rows = $collection->find($qry);
+  
+    foreach($rows as $row)
+        {
+            if($row['Username']== $username)
+            {
+                $criteria = ['Username' => $row['Username']];
+
+                $newData = ['$set' => ['Email' => $email, 'Name'=> $name, 
+                                       'Birthday' => $birthday, 'PhoneNumber' => $phoneNumber, 
+                                       'Gender' => $gender]]; 
+
+
+               if(!empty($email) || !empty($name) || !empty($birthday) || !empty($phoneNumber) || !empty($gender))
+               {
+
+                    $collection -> updateOne($criteria,$newData, array('upsert' => true ));
+                    $usercorrect = true;
+                }
+              else{
+
+                echo json_encode(array('email' => $row['Email']
+                                            ,'name' => $row['Name'],
+                                            'birthday' => $row['Birthday'],
+                                            'phoneNumber'=> $row['PhoneNumber'],
+                                            'gender' => $row['Gender']));
+                    
+               }
+            }
+            
+        }
+        if($usercorrect)
+        {
+          $results = $collection ->find($qry);
+
+          foreach ($results as $result)
+           {
+            
+            if($result['Username']== $username )
+            {
+              echo json_encode(array('email' => $result['Email']
+                                            ,'name' => $result['Name'],
+                                            'birthday' => $result['Birthday'],
+                                            'phoneNumber'=> $result['PhoneNumber'],
+                                            'gender' => $result['Gender']));
+
+            }          
+
+
+          }
+
+          
+
+
+        }
+  
+
+ }else{
+     die("db is not connected");
+ }
+ 
+
+
+?>
